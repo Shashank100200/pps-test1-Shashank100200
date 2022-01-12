350f8155470f71e540ced6c677f6101594c5a48d
Shashank B N <97089480+Shashank100200@users.noreply.github.com>
2022-01-12 04:19:18 +0000

    initiated new program

--- p0final.c
+++ p0final.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
-  printf("my name is shashank \n");
-  return 0;
+  int num1,num2;
+  float sum;
+  printf("enter the two numbers\n");
+   scanf("%d%d",&num1,&num2);
+  sum=num1 +  num2;
+  printf("addition of %d + %d is=%f\n" ,num1,num2,sum);
 }
