ce48cff0e8b628c7526e77ac37847219985049ef
Shashank B N <97089480+Shashank100200@users.noreply.github.com>
2022-01-26 05:58:41 +0000

    debugging of 1st program

---
+++ p2original.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int input();
+{
+printf("enter two numbers\n");
+scanf("%d%d",&num1,&num2);
+return 0;
+}
+{
+int add(int a, int b);
+sum=num1+num2;
+}
+{
+void output(int a, int b, int sum);
+ printf("addition of %d + %d= %f\n",num1,num2,sum);
+}
+int main()
+{
+  int num1,num2;
+  float sum;
+  printf("enter two numbers\n");
+  return 0;
+}
